d7a6474b54fb34dd9d34dc11ae9ae813bdbab535
copea <copea@si.edu>
2014-12-03 19:38:52 +0000

    docs and dimensions stuff

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # py-cooperhewitt-roboteyes-ascii
 
+## Usage
+
+### Simple
+
+	import sys
+	import Image
+
+	import cooperhewitt.roboteyes.ascii as ascii
+
+	path = sys.argv[1]
+	im = Image.open(path)
+
+	print ascii.draw(im)
+
+### Fancy
+
+	import sys
+	import Image
+	import urllib2
+	from cStringIO import StringIO
+	import optparse
+
+	import cooperhewitt.roboteyes.ascii as ascii
+
+	parser = optparse.OptionParser()
+	parser.add_option('--url', dest='url', action='store_true', default=False, help='')
+	parser.add_option('--max', dest='max', action='store', default=80, help='')
+
+	(opts, args) = parser.parse_args()
+
+	path = args[0]
+
+	if opts.url:
+
+		rsp = urllib2.urlopen(path)
+		fh = StringIO(rsp.read())
+		im = Image.open(fh)
+
+	else:
+		im = Image.open(path)
+
+	print ascii.draw(im, max=int(opts.max))
+
+## See also
+
+* http://jwilk.net/software/python-aalib
+
--- cooperhewitt/roboteyes/ascii/__init__.py
+++ cooperhewitt/roboteyes/ascii/__init__.py
@@ -1,18 +1,38 @@
 import aalib
-import Image
-import urllib2
-import logging
 
-def draw(path, **kwargs):
+def dimensions(im, max_dim=300):
 
-    w = kwargs.get('width', 80)
-    h = kwargs.get('height', 40)
+    w,h = im.size
+
+    if w <= max_dim and h <= max_dim:
+        return (w, h)
+
+    if w > h:
+
+        wpercent = (max_dim/float(w))
+
+        w = max_dim
+        h = int((float(h) * float(wpercent)))
+        
+    else:
+
+        hpercent = (max_dim/float(h))
+
+        h = max_dim
+        w = int((float(w) * float(hpercent)))
+
+    return (w, h)
+
+def draw(im, **kwargs):
+
+    m = kwargs.get('max', 80)
+    w,h = dimensions(im, m)
+
+    w = kwargs.get('width', w)
+    h = kwargs.get('height', h)
 
     screen = aalib.AsciiScreen(width=w, height=h)
 
-    fh = open(path, 'rb')
-
-    im = Image.open(fh)
     im = im.convert('L')
     im = im.resize(screen.virtual_size)
 
@@ -22,13 +42,28 @@ def draw(path, **kwargs):
 if __name__ == '__main__':
 
     import sys
-    import os.path
+    import Image
+    import urllib2
+    from cStringIO import StringIO
 
-    path = sys.argv[1]
+    import optparse
 
-    if not os.path.exists(path):
-        print "404"
-        sys.exit(1)
+    parser = optparse.OptionParser()
+    parser.add_option('--url', dest='url', action='store_true', default=False, help='')
+    parser.add_option('--max', dest='max', action='store', default=80, help='')
 
-    print draw(path)
+    (opts, args) = parser.parse_args()
+
+    path = args[0]
+
+    if opts.url:
+
+        rsp = urllib2.urlopen(path)
+        fh = StringIO(rsp.read())
+        im = Image.open(fh)        
+
+    else:
+        im = Image.open(path)
+
+    print draw(im, max=int(opts.max))
     sys.exit(0)
